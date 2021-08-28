cea534197da4294ed62c3ccb2fcfc2dd70b53742
PhD Romance <89710148+phdromance@users.noreply.github.com>
2021-08-28 19:11:13 -0400

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,16 +1,16 @@
 // 1. two sum https://leetcode.com/problems/two-sum/
 
 var twoSum = function (nums, target) {
-	let numObj = { };
+	var numObj = { };
 
 	for (let i = 0; i < nums.length; i++) {
-		numObj[nums[i]] = i;
-	}
-
-	for (let i = 0; i < nums.length; i++) {
-		let num2find = target - nums[i];
+		var num2find = target - nums[i];
 		if (numObj.hasOwnProperty(num2find) && numObj[num2find] != i) {
 			return [i, numObj[num2find]];
+		}
+		else {
+			let key2add = nums[i].toString();
+			numObj[key2add] = i;
 		}
 	}
 };
