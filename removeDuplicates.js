ae3a016b7b394338e8d5d4ba207aae75735dd48c
PhD Romance <89710148+phdromance@users.noreply.github.com>
2021-08-29 19:18:15 -0400

    Create removeDuplicates.js

---
+++ removeDuplicates.js
@@ -0,0 +1,16 @@
+var removeDuplicates = function(nums) {
+    // Note that no extra memory is allowed! Should only modify array in-place
+    
+    // defensive
+    if(nums.length===0 || nums==null) return 0;
+    
+    // loop through the nums from the second number
+    var id = 1;
+    for(let i=1; i<nums.length; i++){
+        if(nums[i]!=nums[id-1]) {
+            nums[id]=nums[i];
+            id++;
+        }
+    }
+    return id;
+}
